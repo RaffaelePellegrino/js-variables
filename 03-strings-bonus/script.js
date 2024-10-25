feca109b0789fa1dc6957b50cda7259a69486fcd
RaffaelePellegrino <raffaele.pellegrino02@gmail.com>
2024-10-25 18:02:18 +0200

    bonus

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -6,23 +6,23 @@ const hello = "hello"; // NON MODIFICARE QUESTA RIGA
 // 1. Cerca un carattere in una stringa
 // Stampa l'INDICE con cui viene trovato
 // il carattere Y nella variabile city
-const cityIndex = 0;
+const cityIndex = city.indexOf("Y");
 console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe
 // Stampa la sottostringa "York" dalla variabile city
-const citySubstring = "";
-console.log(citySubstring); // Output: "York"
+const citySubstring = city.match("York");
 
+console.log(citySubstring); // Output: "York"
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = "";
+const cityReplaced = city.replace("York","Delhi");
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
 // Concatena due stringhe usando la sintassi `${var}`
 // e rendi cityNameConcat uguale a 'New York City'
-const cityNameConcat = "";
+const cityNameConcat = `${city} ${cityName}`;
 console.log(cityNameConcat); // Output: "New York City"
 
 // 5. Tutto in maiscolo
